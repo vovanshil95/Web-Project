cd06572d9297eaceccc973eda44fa32a7332ffad
Geek-o-Byte <mmiwf16@gmail.com>
2021-12-14 17:32:42 +0300

    Frontend moving

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import {DatePicker, Button} from 'antd';
+import {DatePicker, Button, PageHeader, Layout, Menu, Breadcrumb} from 'antd';
 import "antd/dist/antd.css";
 
 const StartApp = () =>{
   return(
+
     <BrowserRouter>
       <div>
-        <ul>
-          <li>dfdfdf</li>
-          <li>dfdfdf</li>
-          <li>ejrooroitj</li>
-        </ul>
+      <Layout>
+      
+      </Layout>
         <Routes>
           <Route path='login' element={<DatePicker/>}/>
           <Route path='register' element={<Button type="primary">fgfgfgfg</Button>}/>
